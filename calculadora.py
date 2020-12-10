c145d4f4b651b898a6a878ca7fe43e7890f25070
barbichola0899 <barbichola0899@users.noreply.github.com>
2020-12-10 20:29:38 -0300

    basic calculator

---
+++ calculadora.py
@@ -0,0 +1,39 @@
+import math
+
+
+class Calculator(object): #Class calculator 
+    def __init__(self): #Stores all attributes of class
+        pass 
+
+    def sum(self,a,b): 
+        #method sum
+        # Receives:
+        # (a:int or float, b:int or float)
+        # Returns:
+        # a+b:int or float
+
+        return a+b
+    
+    def sub(self,a,b):
+        #method sub
+        # Receives:
+        # (a:int or float, b:int or float)
+        # Returns:
+        # a-b:int or float
+        return a-b
+
+    def mult(self,a,b):
+        #method sub
+        # Receives:
+        # (a:int or float, b:int or float)
+        # Returns:
+        # a-b:int or float
+        return a*b
+
+    def div(self,a,b):
+        #method sub
+        # Receives:
+        # (a:int or float, b:int or float)
+        # Returns:
+        # a-b:int or float
+        return a/b
